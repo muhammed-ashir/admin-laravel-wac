cb5cbbd62e4183b9ab1345f08139181336c931eb
Muhammed Ashir <ashir@webandcrafts.in>
2021-07-15 22:52:53 +0530

    logic changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,16 +1,16 @@
 
-function viewBtn(name,email,photo,address,department,designation,status){
-    document.getElementById('name').innerHTML=name;
-    document.getElementById('designation').innerHTML=designation;
+// function viewBtn(name,email,photo,address,department,designation,status){
+//     document.getElementById('name').innerHTML=name;
+//     document.getElementById('designation').innerHTML=designation;
 
-    if(status == 1){
-        document.getElementById('status').innerHTML = '<span style="color: green">Active &nbsp;<i class="fa fa-user-check"></i></span>';
-    }
-    else{
-        document.getElementById('status').innerHTML = '<span style="color: red">Blocked &nbsp;<i class="fa fa-user-times"></i></span>';
-    }
-    document.getElementById('department').innerHTML=department;
-    document.getElementById('email').innerHTML=email;
-    document.getElementById('address').innerHTML=address;
-    document.getElementById('photo').src="http://127.0.0.1:8000/images/"+photo;
-}
+//     if(status == 1){
+//         document.getElementById('status').innerHTML = '<span style="color: green">Active &nbsp;<i class="fa fa-user-check"></i></span>';
+//     }
+//     else{
+//         document.getElementById('status').innerHTML = '<span style="color: red">Blocked &nbsp;<i class="fa fa-user-times"></i></span>';
+//     }
+//     document.getElementById('department').innerHTML=department;
+//     document.getElementById('email').innerHTML=email;
+//     document.getElementById('address').innerHTML=address;
+//     document.getElementById('photo').src="http://127.0.0.1:8000/images/"+photo;
+// }
--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -224,6 +224,23 @@
     <!-- /.content -->
   </div>
   
+<script>
+   
+function viewBtn(name,email,photo,address,department,designation,status){
+    document.getElementById('name').innerHTML=name;
+    document.getElementById('designation').innerHTML=designation;
 
+    if(status == 1){
+        document.getElementById('status').innerHTML = '<span style="color: green">Active &nbsp;<i class="fa fa-user-check"></i></span>';
+    }
+    else{
+        document.getElementById('status').innerHTML = '<span style="color: red">Blocked &nbsp;<i class="fa fa-user-times"></i></span>';
+    }
+    document.getElementById('department').innerHTML=department;
+    document.getElementById('email').innerHTML=email;
+    document.getElementById('address').innerHTML=address;
+    document.getElementById('photo').src="{{ url('images/') }}/"+photo;
+}
+ </script>
 @endsection
 
